bd73c4dfa15a89d4bdd77333e7476e94fadfc8b6
Naysan Saran <naysan.saran@cannforecast.com>
2020-11-29 14:21:14 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -4,13 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name    = "pygeometry-pkg",
-    version = "0.0.1",
+    name    = "pyGeom2D",
+    version = "0.1.1",
     author  = "Naysan Saran",
     author_email    = "naysan.saran@gmail.com",
     description     = "A package for 2D geometry in Python",
-    url             = "https://github.com/NaysanSaran/pygeometry.git",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url             = "https://github.com/NaysanSaran/pyGeom2D.git",
+    license         = "MIT",
     packages        = setuptools.find_packages(),
+    include_package_data = True,
+    install_requires=["matplotlib", "numpy"],
     classifiers     = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
